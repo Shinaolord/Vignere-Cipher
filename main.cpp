59fe58b1953b8cc493c044c99fa8c0318c151f3d
Shinaolord <48556691+Shinaolord@users.noreply.github.com>
2019-03-14 10:56:32 -0400

    Added decryption

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 
-std::string encrypt(std::string plaintext, std::string key);
+std::string encrypt(std::string plaintext, std::string key,int encryptordecrypt);
 
 
 
 int main(){
-std::cout << "Please input the message, then the key, separated by a comma" << std::endl;
+std::cout << "Please input the message, then the key, separated by a comma, and then after another comma, either true=encrypt or false=decrypt" << std::endl;
 std::string plaintext,key;
- while(std::getline(std::cin,plaintext, ',') && std::getline(std::cin,key)){
-        std::cout << encrypt(plaintext,key) << std::endl;
+int enorde;
+ while(std::getline(std::cin,plaintext, ',') && std::getline(std::cin,key,',') && std::cin>>enorde){
+        std::cout << encrypt(plaintext,key,enorde) << std::endl;
+        std::cin.ignore(1,'\n');
 
 }
 
--- vignere.cpp
+++ vignere.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 #include <cstdlib>
 #include<algorithm>
-std::string encrypt(std::string plaintext, std::string key){
+std::string encrypt(std::string plaintext, std::string key, int encryptordecrypt){
 std::string ciphertext="";
+std::string dekey="";
     if(key[0]==' '){
        key.erase(std::remove(key.begin(),key.end(),' '),key.end()); }
        unsigned int keylength=key.length(); /* Gets rid of leading spaces */
-    for( unsigned int i=0; i<plaintext.length();i++){
+for( unsigned int i=0; i<plaintext.length();i++){
         if(islower(plaintext[i])){
             plaintext[i]=toupper(plaintext[i]);
         };
-        if(islower(key[i])){
-            key[i]=toupper(key[i]);
+        if(islower(key[i%keylength])){
+            key[i]=toupper(key[i%keylength]);
         };
+    if(encryptordecrypt>0){
+            if(plaintext[i]==' '){
+                ciphertext+=' ';
+            }
+            else{
 
-        if(plaintext[i]==' '){
-            ciphertext+=' ';
-        }
-        else{
                 div_t ptxtdiv= div(plaintext[i]+key[i%keylength]-65*2,26);
-        std::cout << ptxtdiv.rem <<std::endl;
                 int rem= ptxtdiv.rem;
                 ciphertext+= char (rem+65);
         };
+    }
+   else if(encryptordecrypt<0){
+            if(plaintext[i]==' '){
+                ciphertext+=' ';
+            }
+            else{
+                dekey+=26-key[i%keylength]+65*2;
+                div_t ptxtdiv= div(plaintext[i]+dekey[i%keylength]-65*2,26);
+                int rem= ptxtdiv.rem;
+                ciphertext+= char (rem+65);
+                    };
+        };
 
-    };
+
+};
 return ciphertext;
 }
