c70fc16f452e84bf66cba19d151da80371819342
Shinaolord <48556691+Shinaolord@users.noreply.github.com>
2019-03-14 09:07:03 -0400

    Add files via upload
    Changed std::cout, fixed spelling errors.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ std::string encrypt(std::string plaintext, std::string key);
 
 
 int main(){
-std::cout << "Please input the message, then the key, seperated by a comma (If the key is >1 word, remove the whitespaces" << std::endl;
+std::cout << "Please input the message, then the key, separated by a comma" << std::endl;
 std::string plaintext,key;
  while(std::getline(std::cin,plaintext, ',') && std::getline(std::cin,key)){
         std::cout << encrypt(plaintext,key) << std::endl;
