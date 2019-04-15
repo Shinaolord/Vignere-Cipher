795dc202e9d4f9c6535d774b76360f923abcce92
Shinaolord <tjmelka@mtu.edu>
2019-04-15 16:54:19 -0400

    Updated for longer strings

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@ std::string encrypt(std::string plaintext, std::string key,int encryptordecrypt)
 
 
 int main(){
-std::cout << "INSTRUCTIONS:: function used is of the form FUNCTION(plaintext,key,encryptordecrypt)\n. Plaintext and key are strings, they are seperated by a comma.\n If you include spaces in your key,";
-std::cout<< "please put a space between the comma and the key\n ( Use the form (plaintext, key) if key contains spaces. For example, the input (attack at dawn, secret key) is a valid\n";
-std::cout<<"Input for plaintext and key, respectively).\n If key has no spaces, the first space is irrelevant.";
+std::cout << "INSTRUCTIONS:: Please do not include numbers in the inputs, they do not work properly. Write them out in plain english \n if you must include them.";
+std::cout << "function used is of the form FUNCTION(plaintext,key,encryptordecrypt)\n. Plaintext and key are strings, they are separated by a comma.\n";
+std::cout<< " Spaces in keys are removed in this implementation. For example, the input (attack at dawn, secret key) is a valid\n";
+std::cout<<"Input for plaintext and key, respectively, and will be interpreted as (attack at dawn,secretkey) \n";
 std::cout<<" To encrypt, set encryptordecrypt >0. To decrypt, set it <0." <<std::endl;
 std::string plaintext,key;
 int enorde;
--- vignere.cpp
+++ vignere.cpp
@@ -4,15 +4,24 @@
 std::string encrypt(std::string plaintext, std::string key, int encryptordecrypt){
 std::string ciphertext="";
 std::string dekey="";
-    if(key[0]==' '){
-       key.erase(std::remove(key.begin(),key.end(),' '),key.end()); }
-       unsigned int keylength=key.length(); /* Gets rid of leading spaces */
+/*
+Moves all spaces to the end of the string and returns the position of the first space (ie "1 0 1 0 0 0 1" will modify
+*the string to be "1010001     "  and then return the iterator of 7. This is the end of std::remove. key.erase then deletes everything
+*from the iterator returned by std::remove to the end of the string
+*/
+    key.erase(std::remove(key.begin(), key.end(), ' '), key.end());
+    unsigned int keylength=key.length();
+            for(unsigned int j=0;j<keylength;j++){
+    dekey[j]=div(26-(toupper(key[j])-65),26).rem +65;
+      std::cout<< int (toupper(key[j)])<<" Is the value of key, " << int(dekey[j]-65)<<"\n"<<std::endl;  };
+
+     /* WHY DOESN'T THE DECRYPT WORK WHEN plaintext.length() >51 ??????? TRY MOVING  DEKEY GEN OUT OF for LOOP?? */
 for( unsigned int i=0; i<plaintext.length();i++){
         if(islower(plaintext[i])){
             plaintext[i]=toupper(plaintext[i]);
         };
-        if(islower(key[i%keylength])){
-            key[i]=toupper(key[i%keylength]);
+        if(islower(key[i])){
+            key[i]=toupper(key[i]);
         };
     if(encryptordecrypt>0){
             if(plaintext[i]==' '){
@@ -21,8 +30,8 @@ for( unsigned int i=0; i<plaintext.length();i++){
             else{
 
                 div_t ptxtdiv= div(plaintext[i]+key[i%keylength]-65*2,26);
-                int rem= ptxtdiv.rem;
-                ciphertext+= char (rem+65);
+                int rema= ptxtdiv.rem;
+                ciphertext+= char (rema+65);
         };
     }
    else if(encryptordecrypt<0){
@@ -30,12 +39,11 @@ for( unsigned int i=0; i<plaintext.length();i++){
                 ciphertext+=' ';
             }
             else{
-                dekey[i]=div(26-(key[i]-65),26).rem+65;
-                int rem= div(plaintext[i]+dekey[i%keylength]-65*2,26).rem;
 
-                ciphertext+= char (rem+65);
-                    };
-        };
+                int rema= div(plaintext[i]+dekey[i%keylength]-65*2,26).rem;
+                ciphertext+= char (rema+65);
+                };
+    };
 
 
 };
