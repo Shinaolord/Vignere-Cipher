31e261d015fe5a54f44757e22f45df9892b40a57
Shinaolord <tjmelka@mtu.edu>
2019-12-29 21:58:00 -0500

    Edited, Improved UI
    Added switch() case, error handling for 0 enorde input

--- vignere.cpp
+++ vignere.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <cstdlib>
-#include<algorithm>
+#include <string>
+#include <algorithm>
+
+
+
 std::string encrypt(std::string plaintext, std::string key, int encryptordecrypt){
 std::string ciphertext="";
 std::string dekey="";
@@ -9,40 +13,38 @@ Moves all spaces to the end of the string and returns the position of the first 
 *the string to be "1010001     "  and then return the iterator of 7. This is the end of std::remove. key.erase then deletes everything
 *from the iterator returned by std::remove to the end of the string
 */
-    key.erase(std::remove(key.begin(), key.end(), ' '), key.end());
-    unsigned int keylength=key.length();
-            for(unsigned int j=0;j<keylength;j++){
-    dekey[j]=div(26-(toupper(key[j])-65),26).rem +65;
-      std::cout<< int (toupper(key[j)])<<" Is the value of key, " << int(dekey[j]-65)<<"\n"<<std::endl;  };
-
-     /* WHY DOESN'T THE DECRYPT WORK WHEN plaintext.length() >51 ??????? TRY MOVING  DEKEY GEN OUT OF for LOOP?? */
-for( unsigned int i=0; i<plaintext.length();i++){
+ key.erase(std::remove(key.begin(), key.end(), ' '), key.end());
+ unsigned int keylength=key.length();
+    for(unsigned int j=0;j<keylength;j++){
+        dekey[j]=div(26-(toupper(key[j])-65),26).rem +65;
+    };
+    for( unsigned int i=0; i<plaintext.length();i++){
         if(islower(plaintext[i])){
             plaintext[i]=toupper(plaintext[i]);
         };
-        if(islower(key[i])){
+if(islower(key[i])){
             key[i]=toupper(key[i]);
         };
     if(encryptordecrypt>0){
-            if(plaintext[i]==' '){
-                ciphertext+=' ';
+        if(plaintext[i]==' '){
+             ciphertext+=' ';
             }
-            else{
+        else{
 
-                div_t ptxtdiv= div(plaintext[i]+key[i%keylength]-65*2,26);
-                int rema= ptxtdiv.rem;
-                ciphertext+= char (rema+65);
+             div_t ptxtdiv= div(plaintext[i]+key[i%keylength]-65*2,26);
+             int rema= ptxtdiv.rem;
+             ciphertext+= char (rema+65);
         };
     }
-   else if(encryptordecrypt<0){
-            if(plaintext[i]==' '){
-                ciphertext+=' ';
+else if(encryptordecrypt<0){
+        if(plaintext[i]==' '){
+             ciphertext+=' ';
             }
-            else{
+        else{
 
-                int rema= div(plaintext[i]+dekey[i%keylength]-65*2,26).rem;
-                ciphertext+= char (rema+65);
-                };
+             int rema= div(plaintext[i]+dekey[i%keylength]-65*2,26).rem;
+             ciphertext+= char (rema+65);
+             };
     };
 
 
